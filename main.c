2e902d5081fa005614d6fb036ee40b1497dd8322
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-28 16:35:39 +0400

    1. Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include "Main.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
 
-int main(int argc, const char * argv[]){
-	setlocale(LC_ALL, "Rus");
+//Коновалов Денис
 
-	system("pause");
+
+int DexToBin(int x){
+	int a = div(x, 2);
+	
+	//условие выхода из рекурсии
+	if (x % 2 == 0 && a == 0){
+		printf("\n");
+		return 0;
+	}
+
+	//печатаем результат
+	printf("%d", x % 2);
+
+	return DexToBin(a);
+}
+
+
+int main(int argc, const char * argv[]){
+	int x = 0;
+
+	setlocale(LC_ALL, "Rus");
+	
+	printf("\n---=== 1 ===---\n");
+	do{
+		printf("Введите десятичное число для перевода в двоичную СС. 0 - выход\n");
+		printf("Чтение результата справа-налево\n");
+		scanf("%d", &x);
+		
+		if (x != 0) DexToBin(x);
+	}while (x != 0);
+
+	//system("pause");
 	return 0;
 }
