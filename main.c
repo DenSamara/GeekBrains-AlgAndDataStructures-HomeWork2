da60c3a616c2e680ae6157753597dc23cffeb963
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-28 16:01:13 +0400

    Создал пустой проект

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "Main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(int argc, const char * argv[]){
+	setlocale(LC_ALL, "Rus");
+
+	system("pause");
+	return 0;
+}
