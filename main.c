bf58ad6d0760fc21e1fe196b3fdbefab2bb017bb
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-01 12:31:19 +0400

    Что-то сделал

--- main.c
+++ main.c
@@ -4,9 +4,16 @@
 
 //Коновалов Денис
 
+int command1(int a){
+	return a+1;
+}
 
-//Итеративная функция возведения a в b
-int myPowIter(int a, int b){
+int command2(int a){
+	return 2*a;
+}
+
+//Итеративная функция Калькулятор
+int myCalcIter(int a, int b){
 	int i, result = 1;
 
 	for(i = 1; i <= b; i++){
@@ -16,53 +23,52 @@ int myPowIter(int a, int b){
 	return result;
 }
 
-//Рекурсивная функция возведения a в b
-int myPowReq(int a, int b){
+//Рекурсивная функция Калькулятор
+int myCalcReq(int a, int b, int result){
 	//условие выхода из рекурсии
-	if (b <= 0){
-		return 1;
+	if (a >= b){
+		result++;
+		printf(" result: %d\n", result);
+		return result;
 	}
 
-	return a*myPowReq(a, b-1);
+	//так как число, умноженное на 2 больше, чем увеличенное на 1, то сначала выполняем команду 2.
+	if (command2(a) <= b){
+		a = command2(a);
+		if (a < b){
+			printf("2:%d ", a);
+		}
+		result = myCalcReq(a, b, result);
+	}
+	else {
+		if (command1(a) <= b) {
+			a = command1(a);
+			if (a < b){
+				printf("1:%d ", a);
+			}
+			result = myCalcReq(a, b, result);
+		}
+	}
+
+	return myCalcReq(a, b, result);
 }
-
-//Рекурсивная функция возведения a в b с использованием свойства чётности степени
-int myPowReq2(int a, int b){
-	//условие выхода из рекурсии
-	if (b % 2 == 0 && div(b, 2) == 0){
-		return a;
-	}
-
-	if (b % 2){
-		return a * myPowReq2(a, div(b, 2));
-	}
-
-	return myPowReq2(a, div(b, 2));
-}
-
 
 int main(int argc, const char * argv[]){
-	int a = 0, b = 0;
+	int a = 3, b = 20;
 
 	setlocale(LC_ALL, "Rus");
 	
-	printf("\n---=== 2 ===---\n");
-	printf("Введите число для возведения в степень\n");
+	printf("\n---=== 3 ===---\n");
+	printf("Введите начальное число, целое\n");
 	scanf("%d", &a);
-	printf("Введите степень числа\n");
+	printf("Введите конечное\n");
 	scanf("%d", &b);
 		
-	if (a == 0 && b == 0){
-		printf("Циклом: %d\n", 0);
-		printf("Рекурсивно: %d\n", 0);
-		printf("Дихотомически: %d\n", 0);
-	}else{
-		if (b >= 0){
-			printf("Циклом: %d\n", myPowIter(a, b));
-			printf("Рекурсивно: %d\n", myPowReq(a, b));
-			printf("Дихотомически: %d\n", myPowReq2(a, b));//
-		}else
-			printf("Степень числа не может быть отрицательной");
+	if (a < b){
+		printf("Количество программ рекурсией: %d\n", myCalcReq(a, b, 0));
+	}
+	else{
+		printf("Некорректный ввод\n");
 	}
 
 	system("pause");
